7c0037c007a92fee9e6a4ccb73c9359bf2ac211a
yekingyan <529616@gmail.com>
2018-09-14 00:55:32 +0800

    1，7

---
+++ 1_Two Sum.py
@@ -0,0 +1,39 @@
+﻿
+def two_sum1(target, nums):
+    for i in range(len(nums)):
+        # 另一数倒序历遍至i
+        for j in range(len(nums)-1, i, -1):
+            if nums[i] + nums[j] == target and i != j:
+                return [i, j]
+
+
+def two_sum2(target, nums):
+    d = {}
+    for i, num in enumerate(nums):
+        n = target - num
+        if n in d:
+            return [d[target - num], i]
+        d[num] = i
+    return None
+
+
+def two_sum3(target, nums):
+    d = []
+    for i, num in enumerate(nums):
+        n = target - num
+        if n in nums and i != nums.index(n):
+            d.append([i, nums.index(n)])
+            return d[0]
+
+
+def text():
+    nums = [3, 2, 3]
+    target = 6
+    l1 = two_sum1(target, nums)
+    l2 = two_sum2(target, nums)
+    l3 = two_sum3(target, nums)
+    print(l1, l2, l3)
+
+
+if __name__ == '__main__':
+    text()
---
+++ 7_Reverse_Integer.py
@@ -0,0 +1,83 @@
+﻿#!/usr/bin/python3
+# 用于反转正负数
+
+
+# 方法一
+def return_abs(num):
+    # 反转正值数
+    a = str(num)
+    b = len(a)
+    s = 0
+    for i in range(1, b + 1, 1):
+        num1 = num % (10 ** i)
+        num2 = int(num1 / (10 ** (i - 1)))
+        re_num = num2 * 10 ** (b - i)
+        s += re_num
+    return s
+
+
+def return_num(num):
+    if num > 0:
+        re_num = return_abs(num)
+        return re_num
+    else:
+        # 如果为负数则先转为正值
+        num = abs(num)
+        return_abs(num)
+        re_num = return_abs(num)
+        return -re_num
+
+
+# 方法二
+def reverse(x):
+    if x < 0:
+        x = abs(x)
+        x = ''.join(reversed(str(x)))
+        re_num = int('-' + x)
+    else:
+        x = ''.join(reversed(str(x)))
+        re_num = int(x)
+    if re_num < -2**31 or re_num > 2**31:
+        return 0
+    else:
+        return re_num
+
+
+# 方法三
+def reverse3(x):
+    if x < 0:
+        x = abs(x)
+        x = str(x)[::-1]
+        re_num = int('-' + x)
+    else:
+        x = str(x)[::-1]
+        re_num = int(x)
+    if re_num < -2**31 or re_num > 2**31:
+        return 0
+    else:
+        return re_num
+
+
+def test():
+    nums = [
+        (132456789, 987654231),
+        (23456789, 98765432),
+        (12345678, 87654321),
+        (1675555, 5555761),
+        (-132456789, -987654231),
+        (-23456789, -98765432),
+        (-12345678, -87654321),
+        (-1375555, -5555731),
+    ]
+    for num in nums:
+        original, expect = num
+        # exp = return_num(original)
+        exp = reverse(original)
+        # exp = reverse3(original)
+        e = "returnNum error,初始值：({}).计算值：({}).期待值：({})".format(original, exp, expect)
+        assert exp == expect, e
+        # print(returnNum(num))
+
+
+if __name__ == '__main__':
+    print(test())
