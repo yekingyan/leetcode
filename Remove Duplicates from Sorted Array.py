762527e9d4d6d57031bd626cf70da25266bb5fd3
yekingyan <529616@gmail.com>
2019-02-24 21:11:27 +0800

    列表去重

--- Remove Duplicates from Sorted Array.py
+++ Remove Duplicates from Sorted Array.py
@@ -1,8 +1,23 @@
-﻿nums = [0,0,1,1,1,2,2,3,3,4]
-i = 0
-while i < len(nums) - 1:
-    if nums[i] == nums[i + 1]:
-        nums.pop(i)
-    else:
-        i = i + 1
-print(nums)
+﻿
+def unique(nums: list) -> list:
+    i = 0
+    while i < len(nums) - 1:
+        if nums[i] == nums[i + 1]:
+            nums.pop(i)
+        else:
+            i = i + 1
+    return nums
+
+
+def unique2(nums: list) -> list:
+    temp = []
+    for i in nums:
+        if i not in temp:
+            temp.append(i)
+    return temp
+
+
+if __name__ == '__main__':
+    a = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4]
+    b = unique2(a)
+    print(b)
